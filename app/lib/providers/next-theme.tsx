45492924ee2b436378dcae8d469e52a1be949e71
iamadityaanjana <iamadityaanjana@gmail.com>
2024-12-29 21:26:24 +0530

    Resolved hydration error
    Resolved hydration error using dyamic importing of ThemeProvider

--- app/lib/providers/next-theme.tsx
+++ app/lib/providers/next-theme.tsx
@@ -1,8 +1,15 @@
 'use client';
 import * as React from 'react'
-import {ThemeProvider as NextThemeProvider} from 'next-themes';
+// import {ThemeProvider as NextThemeProvider} from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes';
+import dynamic from 'next/dynamic';
 
+const NextThemeProvider = dynamic(
+	() => import('next-themes').then((e) => e.ThemeProvider),
+	{
+		ssr: false,
+	}
+)
 export function ThemeProvider({children,...props}:ThemeProviderProps){
     return(
         <NextThemeProvider {...props}>
