166c0edd66dfb4e0afe98f72883becdacec67a65
iamadityaanjana <iamadityaanjana@gmail.com>
2025-01-07 09:48:14 +0530

    Minor updates
    1. updated url redirection to deployed site instead of localhost

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,7 +18,7 @@ export const formatPrice = (price: Price) => {
 export const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ??
-    'http://localhost:3000/';
+    'slate.adityaanjana.in';
 
   url = url.includes('http') ? url : `https://${url}`;
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
